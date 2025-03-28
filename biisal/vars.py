22a68754f39781788996e9c036f87a41720c79b3
AJisthebest2122 <anshjoshiisthebest@gmail.com>
2025-03-28 10:04:40 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -40,7 +40,7 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://ansh:ansh2122@filetolink.m6ptzlc.mongodb.net/?retryWrites=true&w=majority&appName=FILETOLINK'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'DOWNLOADER ZONE UPDATES')) 
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'DOWNLOADERZONEUPDATES')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @ANSHtheGENIOUS ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
